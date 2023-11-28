91b87c41ac6b11c28e56d1d92ce07386d2e0c16e
pbeesjy <151818730+pbeesjy@users.noreply.github.com>
2023-11-28 11:19:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,11 @@ import snowflake.connector as sf
 
 st.title('피비스 캠페인 내역 관리')
 st.text('캠페인 내역')
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
