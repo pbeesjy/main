c542bf74b03173f0773a962ea3f06fc9a92bf53e
pbeesjy <151818730+pbeesjy@users.noreply.github.com>
2023-11-29 13:00:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,27 @@ streamlit.text("캠페인 내역 확인하기:")
 
 def get_Campaign_list():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from cj.public.Cam_History order by num desc")
-        return my_cur.fetchall()
+        my_cur.execute("SELECT CAM_NAME FROM cj.public.Cam_History ORDER BY CAM_NAME")
+        return [row[0] for row in my_cur.fetchall()]
+
+def update_campaign_url(campaign_name, new_url):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("""
+            UPDATE cj.public.Cam_History
+            SET CAM_URL = %s
+            WHERE CAM_NAME = %s
+        """, (new_url, campaign_name))
+    my_cnx.commit()
+    return f"Updated URL for {campaign_name} to {new_url}"
+
+# Move my_data_rows to the outermost scope
+my_data_rows = []
+
 if streamlit.button('캠페인 List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_Campaign_list()
     my_cnx.close()
-    streamlit.dataframe(my_data_rows)
-    
+    streamlit.write("캠페인명 목록:", my_data_rows)
 
 def insert_row_table(add_1, add_2, add_3, add_4, add_5, add_6, add_7, add_8, add_9):
     with my_cnx.cursor() as my_cur:
@@ -46,27 +59,7 @@ if streamlit.button('업로드'):
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
-
-
-
-
-
-
-
-streamlit.header('캠페인 URL 업데이트')
-
-def update_campaign_url(campaign_name, new_url):
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("""
-            UPDATE cj.public.Cam_History
-            SET CAM_URL = %s
-            WHERE CAM_NAME = %s
-        """, (new_url, campaign_name))
-    my_cnx.commit()
-    return f"Updated URL for {campaign_name} to {new_url}"
-
- 
-
+# 추가: 드롭다운 목록으로 캠페인명 선택
 update_campaign_name = streamlit.selectbox('캠페인명 (업데이트용)', my_data_rows)
 new_url = streamlit.text_input('새로운 URL')
 if streamlit.button('캠페인 업데이트'):
